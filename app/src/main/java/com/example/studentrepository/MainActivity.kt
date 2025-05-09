12c2909edbe1c33b8259087bd1d957a41b8035f2
opkobi29 <idoalk@gmail.com>
2025-05-09 23:38:51 +0300

    new

--- app/src/main/java/com/example/studentrepository/MainActivity.kt
+++ app/src/main/java/com/example/studentrepository/MainActivity.kt
@@ -1,6 +1,9 @@
-package com.example.studentrepository
+package com.idz.colman24class2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // TODO: Step 1 - Add Student Button ✅
+        // TODO: Step 2 - Navigate to AddStudentActivity
+        // TODO: Step 3 - Create AddStudentLayout
+        // TODO: Step 4 - Save Student
+
+        val addStudentButton: Button = findViewById(R.id.main_acitivity_add_student_button)
+        addStudentButton.setOnClickListener {
+            val intent = Intent(this, AddStudentActivity::class.java)
+            startActivity(intent)
         }
     }
 }
