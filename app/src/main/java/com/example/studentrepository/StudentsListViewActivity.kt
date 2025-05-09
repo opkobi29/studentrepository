ce67c0687a36467bee0e6e351ac364867b0f348e
opkobi29 <idoalk@gmail.com>
2025-05-10 00:19:31 +0300

    new

--- app/src/main/java/com/example/studentrepository/StudentsListViewActivity.kt
+++ app/src/main/java/com/example/studentrepository/StudentsListViewActivity.kt
@@ -1,6 +1,10 @@
 package com.example.studentrepository
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,23 @@ class StudentsListViewActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val listview: ListView? = findViewById(R.id.students_list_view)
+    }
+    class StudentAdapter(): BaseAdapter() {
+        override fun getCount(): Int =10
+
+
+        override fun getItem(position: Int): Any {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemId(position: Int): Long {
+            TODO("Not yet implemented")
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            TODO("Not yet implemented")
         }
     }
 }
